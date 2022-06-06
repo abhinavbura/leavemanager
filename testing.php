87e068e51e784edfd83ba746c48453481e1ac4b6
abhinavbura <abhinav.bura@gmail.com>
2022-06-06 22:46:15 +0530

    my branch is nomass

--- testing.php
+++ testing.php
@@ -10,7 +10,6 @@
 <?php
 if(isset($_POST['apply'])){//to run PHP script on submit 
 if(!empty($_POST['check_list'])){
-	echo "be my pet!!";
 	$role="staff";
 	$sql="SELECT FirstName,LastName,emp_id from tblemployees where role='$role'";
 	$query = mysqli_query($conn, $sql) or die(mysqli_error());
