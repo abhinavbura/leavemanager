2ae2eeee5a1991015a1cb4ebd5645351a6e8cfdf
abhinavbura <abhinav.bura@gmail.com>
2022-06-09 20:59:19 +0530

    added delete feature

--- heads/dellev.php
+++ heads/dellev.php
@@ -2,7 +2,7 @@
 <?php include('../includes/config.php')?>
 <?php
 $lid=intval($_GET['leaveid']);
-$sql = "SELECT admin_status from tblleaves where id=:lid";
+$sql = "SELECT admin_status,empid from tblleaves where id=:lid";
 $query = $dbh -> prepare($sql);
 $query->bindParam(':lid',$lid,PDO::PARAM_STR);
 $query->execute();
@@ -12,22 +12,13 @@ if($query->rowCount() > 0)
 foreach($results as $result)
 { 
     $admins=$result->admin_status;
+    $empid=$result->empid;
     
 
 }  }
-if($admins==0)
-{
-    $sss=1;
-    $result = mysqli_query($conn,"update tblleaves set breathing='$sss' where id='$lid'");
 
-	if ($result) {
-	echo "<script>alert('Leave updated Successfully');</script>";
-	} else{
-	die(mysqli_error());
-	}
-}
-elseif ($admins==1) {
-$sql = "SELECT tblemployees.emp_idtblleaves.num_days,tblleaves.LeaveType from tblleaves join tblemployees on tblleaves.empid=tblemployees.emp_id where tblleaves.id=:lid";
+if ($admins==1) {
+$sql = "SELECT tblemployees.emp_id,tblleaves.num_days,tblleaves.LeaveType from tblleaves join tblemployees on tblleaves.empid=tblemployees.emp_id where tblleaves.id=:lid";
 $query = $dbh -> prepare($sql);
 $query->bindParam(':lid',$lid,PDO::PARAM_STR);
 $query->execute();
@@ -37,13 +28,38 @@ if($query->rowCount() > 0)
 foreach($results as $result)
 { 
     $levtyp=$result->LeaveType;
-    $empid=$result->empid;
+    $empid=$result->emp_id;
     $numdays=$result->num_days;
 
 }  }
 $qname=familyName($levtyp);
 
+$sql = "SELECT $qname from tblemployees where emp_id=:empid";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':empid',$empid,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+foreach($results as $result)
+{ 
+    $left=$result->$qname;
 
-
+} 
+$final=$left+$numdays;
+$update=mysqli_query($conn,"update tblemployees set $qname='$final' where emp_id='$empid'");
+if ($update) {
+	echo "<script>alert('Leave updated Successfully');</script>";
+	} else{
+	die(mysqli_error());
+	}
 }
+$sss=1;
+    $result = mysqli_query($conn,"update tblleaves set breathing='$sss' where id='$lid'");
+
+	if ($result) {
+	echo "<script>alert('Leave updated Successfully');</script>";
+	} else{
+	die(mysqli_error());
+	}
+
+header("location: index.php");
 ?>
--- testing.php
+++ testing.php
@@ -3,45 +3,21 @@
 
 <?php 
 
-$cnt=0;
-   $today=date("d-m-Y");
-   $dates=array();
-   $sql = "SELECT fromdate, status from calender";
-	$query = $dbh -> prepare($sql);
-	$query->execute();
-   $results=$query->fetchAll(PDO::FETCH_OBJ);
-   if($query->rowCount() > 0)
-						{
-						foreach($results as $result)
-						{
-                            echo $result->fromdate."<br>";
-                            $daa=strtotime($result->fromdate);
-                            $dates[$daa]=$result->status;
-
-                  } }
-    var_dump($dates);
-	while (strtotime($start_date) <= strtotime($end_date)) {
-        $check=false;
-		$timestamp = strtotime($start_date);
-		echo $timestamp."<br>";
-        $day = date('D', $timestamp);
-		if(array_key_exists($timestamp,$dates))
-        {
-            echo $timestamp."-->".$dates[$timestamp];
-
-            if($dates[$timestamp]!='holiday')
-            {
-                $check=true;
-            }
-        }
-		if($day!="Sun" && !array_key_exists($timestamp,$dates) && $check)
-		{
-			$cnt++;
-		}
-
-		$start_date = date ("Y-m-d", strtotime("+1 days", strtotime($start_date)));
-	}
-	echo $cnt;
+$empid=1;
+$leave='Medical Leave';
+$tblname=familyName($leave);
+$val=3;
+$name=familyName($leave);
+$description='something';
+$count=23;
+$admremarkdate=date('d-m-Y');
+$did=34;
+$sql = "update tblleaves, tblemployees set tblemployees.$name='$count' where tblleaves.empid = tblemployees.emp_id AND tblleaves.id='$did'";
+if ($sql) {
+    echo "<script>alert('Leave updated Successfully');</script>";
+   } else{
+     die(mysqli_error());
+  }
 
 
 
